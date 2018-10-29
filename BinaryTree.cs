2e99734c72b499ccf4f5511cc850c612ec5e6ef7
SrujanKumarGoregey <44336588+SrujanKumarGoregey@users.noreply.github.com>
2018-10-29 17:22:58 +0530

    Update BinaryTree.cs

--- BinaryTree.cs
+++ BinaryTree.cs
@@ -1,67 +1,8 @@
 using System;
 
-namespace BinaryTree
-{
-    class node
-    {
-        public  int data;
-        public node left;
-        public node right;
 
-    }
-
-       
-
-
-
-
-
-
-
-    class Program
-    {
-       
-        public node insert(node root, int data)
-        {
-            if (root == null)
-            {
-                root = new node();
-                root.data = data;
-            }
-            else if (data< root.data)
-            {
-                root.left = insert(root.left, data);
-            }
-            else
-            {
-                root.right = insert(root.right, data);
-            }
-
-            return root;
-        }
-
-        public static void inorder(node root)
-        {
-            if(root!=null)
-            {
-                Console.WriteLine(root.data);
-                inorder(root.left);
-                inorder(root.right);
-            }
-        }
-        static void Main(string[] args)
-        {
-            Program bst = new Program();
-            node root = null;
-            while(true)
-            {
-                Console.WriteLine("Enter Your choice: \n\t 1:insert \n\t 2:inorder \n\t 3:Exit \n");
-                Console.WriteLine("-----------------------------");
-                var choice = Console.ReadLine();
-                switch(choice)
-                {
-                    case "1using System;
-
+            
+        
 namespace BinaryTree
 {
     class node
@@ -144,27 +85,8 @@ namespace BinaryTree
     }
 }
 
-                        Console.WriteLine("Enter an element to be inserted:");
-                        string input = Console.ReadLine();
-                        int value = int.Parse(input);
-                        root = bst.insert(root, value);
-                        break;
-                    case "2":
-                        inorder(root);
-                        break;
-                    case "3" :
-                        Environment.Exit(0);
-                        break;
+                      
 
-                        
-                }
-            }
-               
-
-        }
-    }
-}
-
-
+            
 
 
